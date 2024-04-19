03a249c7953e9cf118a6b8dc21f14de69c1e7a85
Carlos01333R <carlosrodriguezanavila0@gmail.com>
2024-04-19 12:17:08 -0500

    sistema de chat

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -106,10 +106,10 @@ export default function Header() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <Link to="/" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
               <img className="h-16 w-45" src="https://axis.curn.edu.co/images/uninunez/png/logotxtteal192.png" alt="" />
-            </a>
+            </Link>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -151,7 +151,7 @@ export default function Header() {
               </div>
               <div className="py-6">
                 <a
-                  href="https://half-length-occurre.000webhostapp.com/curn/php/usuario/consultorio.php"
+                  href="https://half-length-occurre.000webhostapp.com/curn/php/usuario/consultorio.php" target="_blank" rel="noopener noreferrer" 
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Perdir Consulta
